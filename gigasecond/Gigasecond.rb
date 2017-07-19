4d41817dafd649cc589ee7f1aa2ad400fdbeb78f
Patricia Arbona <arbonap@gmail.com>
2017-07-19 18:07:43 -0500

    Convert tabs to spaces

--- gigasecond/Gigasecond.rb
+++ gigasecond/Gigasecond.rb
@@ -1,12 +1,9 @@
-require 'date'
-require 'time'
-
 class Gigasecond
-    def self.from (birthday)
-        birthday + 10**9
-    end
+  def self.from birthday
+      birthday + 10**9
+  end
 end
 
 module BookKeeping
-    VERSION = 3
+  VERSION = 3
 end

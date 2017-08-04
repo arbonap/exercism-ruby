ac86d844ffdd0a09088192cea18b49edff29cad2
Patricia Arbona <arbonap@gmail.com>
2017-08-03 23:44:04 -0700

    Complete Grains kata

---
+++ grains/grains.rb
@@ -0,0 +1,24 @@
+# There are 64 squares on a chessboard.
+#
+# Write code that shows:
+# - how many grains were on each square, and
+# - the total number of grains
+
+class Grains
+  def self.square num
+    raise ArgumentError if num <= 0 || num >= 65
+    2 ** (num - 1)
+  end
+
+  def self.total
+    chessboard = []
+    (1..64).map do |i|
+            chessboard.push(self.square(i))
+    end
+    chessboard.reduce(&:+)
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end

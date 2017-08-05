b0bb09523f2be03ac513c0f912d21f2c479ac9b3
Patricia Arbona <arbonap@gmail.com>
2017-08-04 17:26:10 -0700

    17 tests passing

--- bob/bob.rb
+++ bob/bob.rb
@@ -4,10 +4,15 @@ class Bob
     return "Sure." if non_letters_with_question?(txt)
     return 'Whoa, chill out!' if all_upcase?(txt) unless all_nums?(txt)
     return "Sure." if txt.chars.last == "?"
+    return "Fine. Be that way!" if blank?(txt)
     "Whatever."
   end
 
   private
+
+  def self.blank? txt
+    txt.strip&.empty?
+  end
 
   def self.non_letters_with_question? txt
     txt.split(/\W+/).length == 0  && txt.chars.last == "?"

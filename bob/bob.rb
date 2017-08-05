24d43e9371d293cf9641219d7345162e64f212ef
Patricia Arbona <arbonap@gmail.com>
2017-08-04 18:04:35 -0700

    Make Bob class more readable

--- bob/bob.rb
+++ bob/bob.rb
@@ -3,12 +3,16 @@ class Bob
   def self.hey txt
     return "Sure." if non_letters_with_question?(txt)
     return 'Whoa, chill out!' if all_upcase?(txt) unless all_nums?(txt)
-    return "Sure." if txt.strip.chars.last == "?"
+    return "Sure." if last_char_is_question_mark?(txt)
     return "Fine. Be that way!" if blank?(txt)
     "Whatever."
   end
 
   private
+
+  def self.last_char_is_question_mark?(txt)
+    txt.strip.chars.last == "?"
+  end
 
   def self.blank? txt
     txt.strip&.empty?

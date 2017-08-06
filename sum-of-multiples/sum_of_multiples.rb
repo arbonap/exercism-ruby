d9b854fa23700de7fd09655f2c7db9471efb3c2a
Patricia Arbona <arbonap@gmail.com>
2017-08-06 00:23:08 -0700

    One failing spec

---
+++ sum-of-multiples/sum_of_multiples.rb
@@ -0,0 +1,16 @@
+class SumOfMultiples
+  attr_reader :multiple_one, :multiple_two, :multiple_three
+  def initialize(multiple_one=nil, multiple_two=nil, multiple_three=nil)
+    @multiple_one = multiple_one
+    @multiple_two = multiple_two
+    @multiple_three = multiple_three
+  end
+
+  def to(num)
+    # return 0 if num < multiple_one || num < multiple_two || num < multiple_three
+    one = (1..num).select{ |i| i % multiple_one == 0 }.reduce(&:+)
+    two = (1..num).select{ |i| i % multiple_two == 0}.reduce(&:+)
+    # three = (1..num).select{ |i| i % multiple_three == 0}.reduce(&:+)
+    one.to_i + two.to_i
+  end
+end

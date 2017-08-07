2cb671c50275c89a9a7b4b407846463b495a45c5
Patricia Arbona <arbonap@gmail.com>
2017-08-06 18:46:23 -0700

    5 tests pass~~~

--- grade-school/grade_school.rb
+++ grade-school/grade_school.rb
@@ -11,6 +11,15 @@ class School
   end
 
   def students grade
-    school_db[grade]
+    school_db[grade].sort #sort this list
   end
+
+  # def sort
+  #   school_db.sort.each_with_object({}) do |grade, student, sorted|
+  #
+  #     sorted[grade] = student.sort!
+  #   end
+  # end
+  # require 'pry'; binding.pry
+
 end
